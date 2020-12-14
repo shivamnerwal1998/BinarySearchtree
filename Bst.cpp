f5c6176efac8890468e6870ba4065bd66ff99f3c
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-12-14 19:52:23 +0530

    Update Bst.cpp
    This code conains  the various operations on bst  :
    1 . Insert a node
    2. delete a node 
    3. find smallest element 
    4. inorder traversal

--- Bst.cpp
+++ Bst.cpp
@@ -1,190 +1,126 @@
 #include<iostream>
-using namespace std;
-struct bst{
-    int info;
-    struct bst *left;
-    struct bst *right;
-};
-typedef struct bst bst;
+using namespace std ;
 
-void createEmptyTree(bst **tree)
+
+/* self-Referential structure of binary search tree  */
+
+typedef struct bst {
+    int data ;
+    struct bst *left ;
+    struct bst *right ;
+}bst ;
+
+
+/* Function to create empty binary search tree  */
+void createEmpty(bst **tree)
 {
-    *tree = NULL;
+    *tree = NULL ;
 
 }
-// Using queue for level order traversal   
 
-/*
-
-typedef struct NodeType
+/* function to insert node in binary search tree  */
+void insertNode(bst **tree,int info)
 {
-    int info ;
-    struct NodeType *next ;
-
-}node;
-typedef struct queueType
-{
-    node *front ;
-    node *rear ;
-}queue ;
-
-void createQueue(queue *q)
-{
-    q->front = NULL ;
-    q->rear = NULL ;
-
-}
-void enqueue(queue *q,int data)
-{
-    node *ptr;
-    ptr = (node *)malloc(sizeof(node));
-    ptr->info = data ;
-    ptr->next = NULL ;
-    if( q->rear == NULL )
-    {
-        q->front = q->rear = ptr ;
-    }
-    else
-    {
-        (q->rear)->next = ptr ;
-        q->rear = ptr ;
-
-    }
-}
-
-void dequeue(queue *q)
-{
-    node *ptr ;
-    ptr = q->front ;
-    if( q->front ==  NULL && q->rear == NULL  )
-    {
-        printf("\nQUEUE IS EMPTY\n");
-        return ;
-    }
-    (q->front) = (q->front)->next ;
-    printf("%d\n",ptr->info) ;
-    free(ptr) ;
-}
-*/
-
-/*Iterative function to insert element in Binary Search Tree */
-void insertElement(bst **tree,int element)
-{
-    bst *ptr , *node , *parent ;
-    ptr = (bst*)malloc(sizeof(bst)) ;
-    ptr->info = element ;
-    ptr->left = ptr->right = NULL ;
-    if(*tree == NULL){
+    bst *ptr , *node  , *parent;
+    ptr = (bst*) malloc(sizeof(bst)) ;
+    ptr->data = info ;
+    ptr->left = NULL ;
+    ptr->right = NULL ;
+    if(*tree == NULL ){
         *tree = ptr ;
-        return ;
+        return  ;
     }
-    node = (*tree) ;
-    parent = NULL ;
-    while(node != NULL)
-    {
-        parent = node ;
-        if(element < parent->info)
-            node = node -> left ;
-        else
-            node = node -> right ;
-    }
-    if(element < parent -> info)
-        parent->left = ptr;
     else
-        parent->right = ptr ;
-}
-
-/* Recursive function to insert element in binary search tree*/
-void insertElementRecursive(bst **tree,int element)
-{
-     bst *ptr ;
-     ptr = (bst*)malloc(sizeof(bst));
-     ptr->info = element ;
-     ptr->left = ptr->right =NULL ;
-     if(*tree == NULL)
-     {
-        *tree = ptr;
-     }
-     else{
-            if(element < (ptr->info))
-                insertElementRecursive(&((*tree)->left),element);
+    {
+        parent = NULL;
+        node = (*tree) ;
+        while(node != NULL )
+        {
+            parent = node ;
+            if(info < node-> data )
+                node = node -> left ;
             else
-                insertElementRecursive(&((*tree)->right),element);
-
-     }
-
-}
-/*
-    preorder traversal :
-    root
-    left
-    right
-*/
-void preOrderTraversal(bst *tree)
-{
-    if(tree != NULL)
-    {
-        printf("%d\t",tree->info) ;
-        preOrderTraversal(tree->left);
-        preOrderTraversal(tree->right);
+                node = node -> right ;
+        }
     }
-
-}
-/*
-    post order traversal :
-    left
-    right
-    root
-*/
-void postOrderTraversal(bst *tree)
-{
-    if(tree != NULL)
-    {
-        postOrderTraversal(tree->left);
-        postOrderTraversal(tree->right);
-        printf("%d\t",tree->info) ;
-    }
-
+        if (info < (parent -> data) )
+                parent ->left = ptr ;
+        else
+            parent -> right = ptr ;
 }
 
-/* Inorder traversal :
-    left
-    root
-    right
-*/
-/* inorder trasversal displays the elements of b.s.t. in sorted form
-*/
-
+/* InOrder Traversal of binary search tree */
 void inOrderTraversal(bst *tree)
 {
-    if(tree != NULL)
+    if(tree != NULL )
     {
-        inOrderTraversal(tree->left);
-        printf("%d\t",tree->info) ;
-        inOrderTraversal(tree->right);
+        inOrderTraversal(tree->left ) ;
+        cout << tree->data << "\t" ;
+        inOrderTraversal(tree->right) ;
     }
 
 }
+/* Function to find smallest element in binary search tree */
 
-/* main function */
+bst* minValue(bst*tree)
+{
+    while(tree != NULL && tree->left != NULL)
+        tree = tree->left ;
+    return tree ;
+}
+
+
+/*Function to Delete a  Node in Binary Search Tree */
+void deleteNode(bst **tree ,  int element )
+{
+    bst *temp ;
+    if(*tree == NULL )
+        return ;
+    else if(element < (*tree)->data)
+        deleteNode( &((*tree)->left) , element ) ;
+    else if(element > (*tree)->data)
+        deleteNode(&((*tree)->right), element) ;
+    else
+    {
+        if( (*tree)->left == NULL  )
+        {
+            temp = (*tree) ;
+            (*tree) = (*tree)->right ;
+            free(temp) ;
+        }
+        else if( (*tree)->right == NULL  )
+        {
+            temp = (*tree) ;
+            (*tree) = (*tree)->right ;
+            free(temp) ;
+        }
+        else
+        {
+            temp = minValue((*tree)->right);
+            (*tree)->data = temp ->data ;
+            deleteNode(&(*tree)->right,temp->data) ;
+
+        }
+
+    }
+}
+
+
 int main()
 {
-    bst *root;
-    createEmptyTree(&root);
-    //insertElement();
-    insertElementRecursive(&root,10);
-    insertElementRecursive(&root,20);
-    insertElementRecursive(&root,5);
-    insertElementRecursive(&root,15);
-    insertElementRecursive(&root,30);
-
-
-
-    cout<<"\nTraversing the tree in pre order\n";
-    preOrderTraversal(root);
-    printf("\n");
-    postOrderTraversal(root);
-    printf("\n");
-    inOrderTraversal(root);
-    return 0;
+    bst *tree ;
+    bst *ptr ;
+    createEmpty(&tree);
+    insertNode(&tree,10);
+    insertNode(&tree,5);
+    insertNode(&tree,4);
+    insertNode(&tree,3);
+    insertNode(&tree,15);
+    insertNode(&tree,20);
+    insertNode(&tree,30);
+    inOrderTraversal(tree);
+    deleteNode(&tree , 10) ;
+    cout << endl ;
+    inOrderTraversal(tree);
+    getchar();
 }
